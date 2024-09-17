8cc271eaf58d289f214cc8ad14d3efca4ba25215
shevi <shev.igor2002@yandex.ru>
2024-09-17 17:26:59 +0300

    added column list

---
+++ lib/ui/page/column_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+// Экран с Column
+class ColumnScreen extends StatefulWidget {
+  @override
+  _ColumnScreenState createState() => _ColumnScreenState();
+}
+
+class _ColumnScreenState extends State<ColumnScreen> {
+  List<String> _items = ['Пользователь 1', 'Пользователь 2', 'Пользователь 3'];
+
+  void _addItem() {
+    setState(() {
+      _items.add('Пользователь ${_items.length + 1}');
+    });
+  }
+
+  void _removeItem(int index) {
+    setState(() {
+      _items.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+              children: _items
+                  .asMap()
+                  .entries
+                  .map((entry) => ListTile(
+                        title: Text(entry.value),
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () => _removeItem(entry.key),
+                        ),
+                      ))
+                  .toList(),
+            ),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(onPressed: _addItem, child: Text('Добавить')),
+          ],
+        ),
+        SizedBox(height: 25,),
+      ],
+    );
+  }
+}
--- lib/ui/page/home_screen.dart
+++ lib/ui/page/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pr4/ui/page/column_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({super.key, required this.title});
@@ -11,6 +12,10 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    ColumnScreen(),
+  ];
 
   void _onItemTapped(int index) {
     setState(() {
@@ -26,9 +31,7 @@ class _HomeScreenState extends State<HomeScreen> {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text(widget.title),
         ),
-        body: const Center(
-          child: Text('Hello World!'),
-        ),
+        body: _screens[_selectedIndex],
 
         // BottomNavigationBar
         bottomNavigationBar: BottomNavigationBar(
